c1b854ee08a0d7cdb0b42ec74390dbc7f489c88d
Stefan Wille <github@stefanwille.com>
2018-03-24 11:24:09 +0100

    mixed

--- yup-validation.spec.js
+++ yup-validation.spec.js
@@ -1,75 +1,110 @@
-const { object, number, string } = require('yup');
+const yup = require('yup');
 
 describe('yup', () => {
   describe('isValidSync()', () => {
     it('returns whether if the given object is valid', async () => {
-      const schema = number();
+      const schema = yup.number();
       expect(schema.isValidSync(30)).toBe(true);
       expect(schema.isValidSync('NO')).toBe(false);
-    });
-
-    it('validates optional values', () => {
-      const schema = number();
-      expect(schema.isValidSync(undefined)).toBe(true);
-    });
-
-    it('validates required', () => {
-      const schema = number().required();
-      expect(schema.isValidSync(undefined)).toBe(false);
-    });
-
-    it('validates object schemas', () => {
-      const objectSchema = object({
-        name: string().required(),
-        age: number().required()
-      });
-
-      expect(objectSchema.isValidSync({ name: 'jimmy', age: 27 })).toBe(true);
-      expect(
-        objectSchema.isValidSync({ name: 'jimmy', age: 'not-a-number' })
-      ).toBe(false);
     });
   });
 
   describe('isValid()', async () => {
     it('returns a Promise indicating if the given object is valid', async () => {
-      const schema = number();
+      const schema = yup.number();
       expect(await schema.isValid(30)).toBe(true);
       expect(await schema.isValid('NO')).toBe(false);
     });
+  });
 
-    describe('validate()', () => {
-      it('returns a Promise with the converted object on success', async () => {
-        const schema = number();
-        expect(await schema.validate(30)).toBe(30);
-        expect(await schema.validate('30')).toBe(30);
-      });
+  describe('validateSync()', () => {
+    it('returns the converted object on success', () => {
+      const schema = yup.number();
+      expect(schema.validateSync(30)).toBe(30);
+    });
 
-      it('throws on validation failure', async () => {
-        const schema = number();
-        try {
-          await schema.validate('NO');
-          fail();
-        } catch (error) {
-          expect(error.message).toMatch(/must be a `number` type/);
-        }
+    it('throws on validation failure', () => {
+      const schema = yup.number();
+      try {
+        expect(schema.validateSync('boom')).toBe(30);
+        fail();
+      } catch (error) {
+        expect(error.message).toMatch(/must be a `number` type/);
+        expect(error.errors).toEqual([
+          'this must be a `number` type, but the final value was: `NaN` (cast from the value `"boom"`).'
+        ]);
+      }
+    });
+  });
+
+  describe('validate()', () => {
+    it('returns a Promise with the converted object on success', async () => {
+      const schema = yup.number();
+      expect(await schema.validate('30')).toBe(30);
+    });
+
+    it('throws on validation failure', async () => {
+      const schema = yup.number();
+      try {
+        await schema.validate('NO');
+        fail();
+      } catch (error) {
+        expect(error.message).toMatch(/must be a `number` type/);
+      }
+    });
+  });
+
+  describe('schema types', () => {
+    describe('default', () => {
+      it('validates as optional', () => {
+        const schema = yup.number();
+        expect(schema.isValidSync(undefined)).toBe(true);
       });
     });
 
-    describe('validateSync()', () => {
-      it('returns the converted object on success', () => {
-        const schema = number();
-        expect(schema.validateSync(30)).toBe(30);
+    describe('require()', () => {
+      it('validates required', () => {
+        const schema = yup.number().required();
+        expect(schema.isValidSync(undefined)).toBe(false);
       });
+    });
 
-      it('throws on validation failure', () => {
-        const schema = number();
-        try {
-          expect(schema.validateSync('boom')).toBe(30);
-          fail();
-        } catch (error) {
-          expect(error.message).toMatch(/must be a `number` type/);
-        }
+    describe('object()', () => {
+      it('validates object schemas', () => {
+        const objectSchema = yup.object({
+          name: yup.string().required(),
+          age: yup.number().required()
+        });
+
+        expect(
+          objectSchema.isValidSync({
+            name: 'jimmy',
+            age: 27
+          })
+        ).toBe(true);
+        expect(
+          objectSchema.isValidSync({
+            name: 'jimmy',
+            age: 'not-a-number'
+          })
+        ).toBe(false);
+      });
+    });
+
+    describe('array()', () => {
+      it('validates arrays', () => {
+        const schema = yup.array().of(yup.number());
+        expect(schema.isValidSync([1, 2, 3])).toBe(true);
+        expect(schema.isValidSync([])).toBe(true);
+        expect(schema.isValidSync(['NO'])).toBe(false);
+      });
+    });
+
+    describe('mix()', () => {
+      it('validates "any" type', () => {
+        const schema = yup.mixed();
+        expect(schema.isValidSync(3)).toBe(true);
+        expect(schema.isValidSync('YES')).toBe(true);
       });
     });
   });
