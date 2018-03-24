691331b19e07bb16ea480122aa7c505972726c6e
Stefan Wille <github@stefanwille.com>
2018-03-24 10:53:15 +0100

    isValid and validate

--- yup-validation.spec.js
+++ yup-validation.spec.js
@@ -1,19 +1,53 @@
+const { object, number, string } = require('yup');
+
 describe('yup', () => {
-  describe('isValid', () => {
-    it('validates required values', async () => {
-      const contactSchema = object({
+  describe('isValid()', () => {
+    it('returns true if the given object is valid', async () => {
+      const schema = number();
+      expect(await schema.isValid(30)).toBe(true);
+      expect(await schema.isValid('NO')).toBe(false);
+    });
+
+    it('validates required', async () => {
+      const optionalSchema = number();
+      expect(await optionalSchema.isValid(30)).toBe(true);
+      expect(await optionalSchema.isValid(undefined)).toBe(true);
+      const requiredSchema = number().required();
+      expect(await requiredSchema.isValid(30)).toBe(true);
+      expect(await requiredSchema.isValid(undefined)).toBe(false);
+    });
+
+    it('validates object schemas', async () => {
+      const objectSchema = object({
         name: string().required(),
         age: number()
           .required()
           .positive()
           .integer()
       });
-      const contact = {
-        name: 'jimmy',
-        age: 27
-      };
 
-      await contactSchema.isValid(contact);
+      expect(await objectSchema.isValid({ name: 'jimmy', age: 27 })).toBe(true);
+      expect(
+        await objectSchema.isValid({ name: 'jimmy', age: 'not-a-number' })
+      ).toBe(false);
+    });
+
+    describe('validate()', () => {
+      it('returns the converted object on success', async () => {
+        const schema = number();
+        expect(await schema.validate(30)).toBe(30);
+        expect(await schema.validate('30')).toBe(30);
+      });
+
+      it('throws an error if invalid', async () => {
+        const schema = number();
+        try {
+          await schema.validate('NO');
+          fail();
+        } catch (error) {
+          expect(error.message).toMatch(/must be a `number` type/);
+        }
+      });
     });
   });
 });
